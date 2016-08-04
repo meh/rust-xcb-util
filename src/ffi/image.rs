d188f6a49f325e9d9865cdfc17931787e6cbc137
meh <meh@schizofreni.co>
2016-08-04 20:49:28 +0200

    image: add SHM support

--- src/ffi/image.rs
+++ src/ffi/image.rs
@@ -1,5 +1,4 @@
 use xcb::ffi::*;
-use xcb::ffi::shm::*;
 use libc::{c_void, c_int};
 
 #[repr(C)]
@@ -20,13 +19,6 @@ pub struct xcb_image_t {
 	pub data: *mut u8,
 }
 
-#[repr(C)]
-pub struct xcb_shm_segment_info_t {
-	pub shmseg: xcb_shm_seg_t,
-	pub shmid: u32,
-	pub shmaddr: *mut u8,
-}
-
 #[cfg_attr(feature = "static", link(name = "xcb-image", kind = "static"))]
 #[cfg_attr(not(feature = "static"), link(name = "xcb-image"))]
 extern "C" {
@@ -41,8 +33,26 @@ extern "C" {
 	pub fn xcb_image_get_pixel(image: *const xcb_image_t, x: u32, y: u32) -> u32;
 	pub fn xcb_image_convert(src: *const xcb_image_t, dst: *mut xcb_image_t) -> *mut xcb_image_t;
 	pub fn xcb_image_subimage(image: *const xcb_image_t, x: u32, y: u32, width: u32, height: u32, base: *mut c_void, bytes: u32, data: *mut u8) -> *mut xcb_image_t;
-	pub fn xcb_image_shm_put(conn: *mut xcb_connection_t, draw: xcb_drawable_t, gc: xcb_gcontext_t, image: *const xcb_image_t, shminfo: xcb_shm_segment_info_t, src_x: i16, src_y: i16, dest_x: i16, dest_y: i16, src_width: u16, src_height: u16, send_event: u8) -> *mut xcb_image_t;
-	pub fn xcb_image_shm_get(conn: *mut xcb_connection_t, draw: xcb_drawable_t, shminfo: xcb_shm_segment_info_t, x: i16, y: i16, plane_mask: u32) -> c_int;
 	pub fn xcb_image_create_from_bitmap_data(data: *mut u8, width: u32, height: u32) -> *mut xcb_image_t;
 	pub fn xcb_create_pixmap_from_bitmap_data(display: *mut xcb_connection_t, d: xcb_drawable_t, data: *mut u8, width: u32, height: u32, depth: u32, fg: u32, bg: u32, gcp: *const xcb_gcontext_t) -> xcb_pixmap_t;
+}
+
+#[cfg(feature = "shm")]
+use xcb::ffi::shm::*;
+
+#[cfg(feature = "shm")]
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct xcb_shm_segment_info_t {
+	pub shmseg: xcb_shm_seg_t,
+	pub shmid: u32,
+	pub shmaddr: *mut u8,
+}
+
+#[cfg(feature = "shm")]
+#[cfg_attr(feature = "static", link(name = "xcb-image", kind = "static"))]
+#[cfg_attr(not(feature = "static"), link(name = "xcb-image"))]
+extern "C" {
+	pub fn xcb_image_shm_put(conn: *mut xcb_connection_t, draw: xcb_drawable_t, gc: xcb_gcontext_t, image: *const xcb_image_t, shminfo: xcb_shm_segment_info_t, src_x: i16, src_y: i16, dest_x: i16, dest_y: i16, src_width: u16, src_height: u16, send_event: u8) -> *mut xcb_image_t;
+	pub fn xcb_image_shm_get(conn: *mut xcb_connection_t, draw: xcb_drawable_t, image: *mut xcb_image_t, shminfo: xcb_shm_segment_info_t, x: i16, y: i16, plane_mask: u32) -> c_int;
 }
--- src/image.rs
+++ src/image.rs
@@ -1,30 +1,108 @@
+use std::ptr;
+use std::slice;
+
 use xcb;
 use ffi::image::*;
 
-pub struct Image(*mut xcb_image_t);
+pub struct Image {
+	ptr:   *mut xcb_image_t,
+	owned: bool,
+}
 
 impl Image {
+	pub fn annotate(&self) {
+		unsafe {
+			xcb_image_annotate(self.ptr)
+		}
+	}
+
 	pub fn width(&self) -> u16 {
 		unsafe {
-			(*self.0).width
+			(*self.ptr).width
 		}
 	}
 
 	pub fn height(&self) -> u16 {
 		unsafe {
-			(*self.0).height
+			(*self.ptr).height
+		}
+	}
+
+	pub fn format(&self) -> xcb::ImageFormat {
+		unsafe {
+			(*self.ptr).format
+		}
+	}
+
+	pub fn scanline_pad(&self) -> u8 {
+		unsafe {
+			(*self.ptr).scanline_pad
+		}
+	}
+
+	pub fn depth(&self) -> u8 {
+		unsafe {
+			(*self.ptr).depth
+		}
+	}
+
+	pub fn bpp(&self) -> u8 {
+		unsafe {
+			(*self.ptr).bpp
+		}
+	}
+
+	pub fn unit(&self) -> u8 {
+		unsafe {
+			(*self.ptr).unit
+		}
+	}
+
+	pub fn plane_mask(&self) -> u32 {
+		unsafe {
+			(*self.ptr).plane_mask
+		}
+	}
+
+	pub fn byte_order(&self) -> xcb::ImageOrder {
+		unsafe {
+			(*self.ptr).byte_order
+		}
+	}
+
+	pub fn bit_order(&self) -> xcb::ImageOrder {
+		unsafe {
+			(*self.ptr).bit_order
+		}
+	}
+
+	pub fn stride(&self) -> u32 {
+		unsafe {
+			(*self.ptr).stride
+		}
+	}
+
+	pub fn size(&self) -> u32 {
+		unsafe {
+			(*self.ptr).size
+		}
+	}
+
+	pub fn data(&self) -> &[u8] {
+		unsafe {
+			slice::from_raw_parts((*self.ptr).data, (*self.ptr).size as usize)
 		}
 	}
 
 	pub fn put(&mut self, x: u32, y: u32, pixel: u32) {
 		unsafe {
-			xcb_image_put_pixel(self.0, x, y, pixel)
+			xcb_image_put_pixel(self.ptr, x, y, pixel)
 		}
 	}
 
 	pub fn get(&self, x: u32, y: u32) -> u32 {
 		unsafe {
-			xcb_image_get_pixel(self.0, x, y)
+			xcb_image_get_pixel(self.ptr, x, y)
 		}
 	}
 }
@@ -32,14 +110,12 @@ impl Image {
 impl Drop for Image {
 	fn drop(&mut self) {
 		unsafe {
-			xcb_image_destroy(self.0);
-		}
-	}
-}
+			if !self.owned {
+				(*self.ptr).data = ptr::null_mut();
+			}
 
-pub fn annotate(image: &mut Image) {
-	unsafe {
-		xcb_image_annotate(image.0)
+			xcb_image_destroy(self.ptr);
+		}
 	}
 }
 
@@ -51,7 +127,10 @@ pub fn get(c: &xcb::Connection, drawable: xcb::Drawable, x: i16, y: i16, width: 
 			Err(())
 		}
 		else {
-			Ok(Image(ptr))
+			Ok(Image {
+				ptr:   ptr,
+				owned: true,
+			})
 		}
 	}
 }
@@ -59,7 +138,7 @@ pub fn get(c: &xcb::Connection, drawable: xcb::Drawable, x: i16, y: i16, width: 
 pub fn put(c: &xcb::Connection, drawable: xcb::Drawable, gc: xcb::Gcontext, image: &Image, x: i16, y: i16) -> xcb::VoidCookie {
 	unsafe {
 		xcb::VoidCookie {
-			cookie:  xcb_image_put(c.get_raw_conn(), drawable, gc, image.0, x, y, 0),
+			cookie:  xcb_image_put(c.get_raw_conn(), drawable, gc, image.ptr, x, y, 0),
 			conn:    c.get_raw_conn(),
 			checked: false,
 		}
@@ -68,19 +147,138 @@ pub fn put(c: &xcb::Connection, drawable: xcb::Drawable, gc: xcb::Gcontext, imag
 
 pub fn is_native(c: &xcb::Connection, image: &Image) -> bool {
 	unsafe {
-		xcb_image_native(c.get_raw_conn(), image.0, 0) == image.0
+		xcb_image_native(c.get_raw_conn(), image.ptr, 0) == image.ptr
 	}
 }
 
 pub fn to_native(c: &xcb::Connection, image: &Image) -> Option<Image> {
 	unsafe {
-		let ptr = xcb_image_native(c.get_raw_conn(), image.0, 1);
+		let ptr = xcb_image_native(c.get_raw_conn(), image.ptr, 1);
 
-		if ptr == image.0 || ptr.is_null() {
+		if ptr == image.ptr || ptr.is_null() {
 			None
 		}
 		else {
-			Some(Image(ptr))
+			Some(Image {
+				ptr:   ptr,
+				owned: true,
+			})
+		}
+	}
+}
+
+#[cfg(feature = "shm")]
+pub mod shm {
+	use std::ptr;
+	use std::ops::{Deref, DerefMut};
+
+	use xcb;
+	use xcb::ffi::*;
+	use xcb::ffi::shm::*;
+	use ffi::image::*;
+	use libc::{shmget, shmat, shmdt, shmctl, IPC_CREAT, IPC_RMID};
+
+	pub struct Image {
+		conn: *mut xcb_connection_t,
+		base: super::Image,
+		shm:  xcb_shm_segment_info_t,
+	}
+
+	pub fn create(c: &xcb::Connection, depth: u8, width: u16, height: u16) -> Result<Image, ()> {
+		unsafe {
+			let setup  = c.get_setup();
+			let format = try!(setup.pixmap_formats().find(|f| f.depth() == depth).ok_or(()));
+			let image  = xcb_image_create(width, height, xcb::IMAGE_FORMAT_Z_PIXMAP,
+				format.scanline_pad(), format.depth(), format.bits_per_pixel(),
+				setup.bitmap_format_scanline_unit(), setup.image_byte_order() as u32, setup.bitmap_format_bit_order() as u32,
+				ptr::null_mut(), !0, ptr::null_mut());
+
+			if image.is_null() {
+				return Err(());
+			}
+
+			let id = match shmget(0, (*image).size as usize, IPC_CREAT | 0o666) {
+				-1 => {
+					xcb_image_destroy(image);
+					return Err(());
+				}
+
+				id => id
+			};
+
+			let addr = match shmat(id, ptr::null(), 0) {
+				addr if addr as isize == -1 => {
+					xcb_image_destroy(image);
+					shmctl(id, IPC_RMID, ptr::null_mut());
+
+					return Err(());
+				}
+
+				addr => addr
+			};
+
+			let seg = c.generate_id();
+			xcb::shm::attach(c, seg, id as u32, false);
+			(*image).data = addr as *mut _;
+
+			Ok(Image {
+				conn: c.get_raw_conn(),
+
+				base: super::Image {
+					ptr:   image,
+					owned: false,
+				},
+
+				shm:   xcb_shm_segment_info_t {
+					shmseg:  seg,
+					shmid:   id as u32,
+					shmaddr: addr as *mut _,
+				}
+			})
+		}
+	}
+
+	impl Deref for Image {
+		type Target = super::Image;
+
+		fn deref(&self) -> &Self::Target {
+			&self.base
+		}
+	}
+
+	impl DerefMut for Image {
+		fn deref_mut(&mut self) -> &mut Self::Target {
+			&mut self.base
+		}
+	}
+
+	impl Drop for Image {
+		fn drop(&mut self) {
+			unsafe {
+				xcb_shm_detach(self.conn, self.shm.shmseg);
+				shmdt(self.shm.shmaddr as *mut _);
+				shmctl(self.shm.shmid as i32, IPC_RMID, ptr::null_mut());
+			}
+		}
+	}
+
+	pub fn get<'a>(c: &xcb::Connection, drawable: xcb::Drawable, image: &'a mut Image, x: i16, y: i16, plane_mask: u32) -> Result<&'a mut Image, ()> {
+		unsafe {
+			match xcb_image_shm_get(c.get_raw_conn(), drawable, image.base.ptr, image.shm, x, y, plane_mask) {
+				1 => Ok(image),
+				_ => Err(())
+			}
+		}
+	}
+
+	pub fn put<'a>(c: &xcb::Connection, drawable: xcb::Drawable, gc: xcb::Gcontext, image: &'a Image, src_x: i16, src_y: i16, dest_x: i16, dest_y: i16, src_width: u16, src_height: u16, send_event: bool) -> Result<&'a Image, ()> {
+		unsafe {
+			if !xcb_image_shm_put(c.get_raw_conn(), drawable, gc, image.base.ptr, image.shm, src_x, src_y, dest_x, dest_y, src_width, src_height, send_event as u8).is_null() {
+				Ok(image)
+			}
+			else {
+				Err(())
+			}
 		}
 	}
 }
